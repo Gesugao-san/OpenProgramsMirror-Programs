8005f5fdb46fb5be779dde87e4eaee9cc235cd5b
Gesugao-san <gesugao.san@gmail.com>
2022-07-03 01:28:38 +0300

    Add _register_this_repo.sh

---
+++ _register_this_repo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+oppm register Proxima-Project/OpenProgramsMirror-Programs
+oppm list
+oppm install proxima-gesu-progs
+
+which hello_world
+which auto_mount
