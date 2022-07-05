cef8c29d5b248460279ac6d416cf603028a1f032
Gesugao-san <gesugao.san@gmail.com>
2022-07-05 23:54:16 +0300

    Add music.lua

---
+++ src/lua/music.lua
@@ -0,0 +1,19 @@
+
+local note = require('note')
+
+-- frequency
+local start = 60 --20
+local stop = 95 --96 --2000
+local step = 1 --1
+local speed = .05 --0.2
+
+print('Playing notes start.')
+for i = start, stop, step do
+  print('Playing note: ' .. i .. '/' .. stop)
+  note.play(i, speed)
+  if i == stop then
+    break
+  end
+end
+print('Playing notes stop.')
+
