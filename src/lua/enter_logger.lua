11ae675ead680b5a8de8eb7dd28e9a0cffcf6b45
Gesugao-san <gesugao.san@gmail.com>
2022-07-11 15:22:55 +0300

    Add enter_logger.lua

--- src/lua/enter_logger.lua
+++ src/lua/enter_logger.lua
@@ -1,33 +1,19 @@
 
 -- OpenComputers 1.6.1.11
 
---[[ for i = 1, 3, 1 do
-  print(i)
-  os.sleep(1)
-end ]]
---if debug then print('component.getPrimary(target_type): ' .. serialization.serialize(component.getPrimary(target_type))) end
---if debug then print(serialization.serialize(components)); print(serialization.serialize(pairs(components))) end
-
 local term = require('term')
 local component = require("component")
 local serialization = require('serialization')
-local event = require('event')
---local os = require('os')
-
-local fs = require("filesystem")
-local keyboard = require("keyboard")
-local shell = require("shell")
-local text = require("text")
-local unicode = require("unicode")
+local sides = require("sides")
 
 local running = true
+local path = '/home/enter_logger.log'
+
+
 Target1 = nil
-Target2 = nil
-
-
 local function find_component(target_type, need_second)
   if need_second == nil then need_second = false end
-  local debug = true
+  local debug = false
   term.write('Finding component: "' .. target_type .. '"... ')
   if (not component.isAvailable(target_type)) then
     print('Error\nDetails: Component isn\'t available.\nSolution: Check is component name correct, connected to the operated computer case and then try again.')
@@ -41,16 +27,14 @@ local function find_component(target_type, need_second)
     os.exit() --do return end
   end
 
-  local n = 0
+  --[[ local n = 0
   for address, componentType in components do
     n = n + 1
     if (n == 1) then
       Target1 = component.getPrimary(target_type) -- = component.proxy(address) -- = components[n]
-    elseif ((need_second) and (n == 2)) then
-      Target2 = component.proxy(address) --components[n]
-      break
     end
-  end
+  end ]]
+  Target1 = component.getPrimary(target_type) -- = component.proxy(address) -- = components[n]
 
   if (Target1 == nil) then
     print('Error\nDetails: No component found.\nSolution: Check is component name correct, connected to the operated computer case and then try again.')
@@ -58,52 +42,83 @@ local function find_component(target_type, need_second)
     os.exit() --do return end
   end
 
-  if need_second then
-    if (components[1] == nil) then
-      print('Error\nDetails: No component found.\nSolution: Check is component name correct, connected to the operated computer case and then try again.')
-        if debug then print(serialization.serialize(components)) end
-      os.exit() --do return end
-    end
-    Target2 = components[1]
-    print('Ok\nTarget1: Address: ' .. Target1.address .. '\nDebug: ' .. serialization.serialize(Target1))
-    if Target1 ~= nil then print('Target2: Address: ' .. Target2.address .. '\nDebug: ' .. serialization.serialize(Target2)) end
-  else
-    print('Ok\nAddress: ' .. Target1.address .. '\nDebug: ' .. serialization.serialize(Target1))
-  end
+  print('Ok\nAddress: ' .. Target1.address) -- .. '\nDebug: ' .. serialization.serialize(Target1))
   if debug then print(serialization.serialize(components)) end
 
 end
 
 find_component('motion_sensor')
-print('')
+find_component('redstone')
+
+local function write_to_file(path, data)
+  File = io.open(path, 'a')
+  if File == nil then
+    print('Error: Can\'t open file "' .. path .. '".')
+    os.exit() --do return end
+  end
+  local result = File:write(data .. '\n')
+  File:close()
+  if (not result) then
+    print('Error: Can\'t write file "' .. path .. '", data: "' .. data .. '".')
+    os.exit() --do return end
+  end
+end
 
 -- https://ocdoc.cil.li/component:signals#motion_sensor_block
 local function motion(address, relativeX, relativeY, relativeZ, entityName)
-  local a = string.sub(address, 1, 8)
+  --local a = string.sub(address, 1, 8)
+  --local d = os.date()
+  local t = os.time()
   --local vectorSum = x + y + z
   local v = tonumber(string.format("%.3f", math.sqrt(math.abs(relativeX) + math.abs(relativeY) + math.abs(relativeZ)))) --vectorLength
   local x = tonumber(string.format("%.3f", relativeX))
   local y = tonumber(string.format("%.3f", relativeY))
   local z = tonumber(string.format("%.3f", relativeZ))
-  print(a .. ' | ' .. entityName .. ' | ' .. v .. ' | ' .. x .. ' | ' .. y .. ' | ' .. z)
+  print(t .. ' Motion: ' .. entityName)-- .. ' ' .. v .. ' ' .. x .. ' ' .. y .. ' ' .. z)
+  write_to_file(path, t .. ' Motion: ' .. entityName)-- .. ' ' .. v .. ' ' .. x .. ' ' .. y .. ' ' .. z)
+end
+
+Side1 = nil
+Side2 = nil
+local function redstone_changed(address, side, oldValue, newValue, color)
+  local t = os.time()
+  if oldValue == 0 then
+    if ((Side1 == nil) and (Side2 == nil)) then
+      Side1 = side
+    elseif (Side2 == nil) then
+      Side2 = side
+    end
+    print(t .. ' Redstone: ' .. sides[side]) --Side triggered
+    write_to_file(path, os.time() .. ' Redstone: ' .. sides[side])
+  end
+
+  --if ((Side1 ~= nil) and (Side2 ~= nil)) then
+  --  print('Side1: ' .. sides[Side1] .. ', Side2: ' .. sides[Side2])
+  --end
+  --print(address, side, oldValue, newValue, color)
 end
 
 
 --event.ignore('motion')
 --local event_id = event.listen('motion', motion)
-print('Entering loop. Press any key to stop.')
-print('HWID | relative X | relative Y | relative Z | entity Name | vector Length')
+write_to_file(path, '[LOG START - ' .. os.date() ..']')
+write_to_file(path, '[ Time | Thing | Entity Name ]')
+print('Entering loop. Press any key to stop.') -- relative X | relative Y | relative Z | vector Length
+print('[ Time | Thing | Entity Name ]')
 while running do
   --event.shouldInterrupt = function() return false end -- disable Alt+Ctrl+C
   local event, address, arg1, arg2, arg3, arg4 = term.pull()
   -- if address == term.keyboard() or address == term.screen() then local blink = true end
-  if event == "key_down" then
+  if event == 'key_down' then
     print('Stopping...')
+    write_to_file(path, '[LOG END - ' .. os.date() .. ']')
     running = false
-  elseif event == "motion" then --and not readonly 
+  elseif event == 'redstone_changed' then
+    redstone_changed(address, arg1, arg2, arg3, arg4)
+  elseif event == 'motion' then --and not readonly
     motion(address, arg1, arg2, arg3, arg4)
-  else
-    print('Waiting for moving...')
+  --else
+  --  print('Waiting for moving...')
   end
   --os.sleep(1)
   --running = false
