95f06597573c86805384176dd8284de181571d7f
Gesugao-san <gesugao.san@gmail.com>
2022-07-12 22:52:40 +0300

    Update _fast.lua

--- src/lua/_fast.lua
+++ src/lua/_fast.lua
@@ -4,12 +4,15 @@
 C = require('component'); S = require('serialization')
 
 function debug(type)
-  if type == nil then print('no type'); require('os').exit() end
-  if next(C.list(type)) == nil then print('no type in list'); require('os').exit() end
+  if type == nil then print('no "' .. type .. '"'); do return end end
+  if next(C.list(type)) == nil then print('no "' .. type .. '" in list'); do return end end
   X = C.getPrimary(type)
-  if X == nil then print('no getPrimary'); require('os').exit() end
+  if X == nil then print('no getPrimary "' .. type .. '"'); do return end end
   print('type: "' .. type .. '", address: ' .. X.address)
-  print(S.serialize(C.methods(X.address)))
+  M = C.methods(X.address)
+  MM = {}; N=0
+  for k, v in pairs(M) do N = N+1; MM[N] = k end
+  print('Methods: ' .. S.serialize(MM)) -- print(S.serialize(M))
   for method, _ in pairs(C.methods(X.address)) do
     print(method .. ': ' .. C.invoke(X.address, method))
     --print(C.doc(X.address, method))
