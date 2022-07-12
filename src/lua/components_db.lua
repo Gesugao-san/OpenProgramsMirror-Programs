08d5ab3a21ce640f2e328bd37ade80a98e0ea5bf
Gesugao-san <gesugao.san@gmail.com>
2022-07-12 23:53:49 +0300

    Update components_db.lua

--- src/lua/components_db.lua
+++ src/lua/components_db.lua
@@ -1,21 +1,49 @@
 
 -- Lua 5.2; OpenComputers 1.6.1.11;
 
-Standart = {
-  'keyboard',
-  'disk_drive',
-  'computer',
+Critical = {
+  'computer', -- Case Mk. 1, 2, 3
   'filesystem',
-  'gpu',
   'eeprom',
+  'gpu',
+}
+
+Input = {
   'screen',
+  'keyboard',
 }
 
 Additional = {
+  'assembler',
+  'charger',
+  'disk_drive',
+  'geolizer',
+  'hologram',
+  'internet',
+  'inventory_controller',
+  'microcontroller',
+  'motion_sensor',
+  'printer3d',
+  'raid', -- as 'filesystem'
   'redstone',
+  'relay',
+  'transposer',
+}
+
+NotShows = {
+  'adapter',
+  'cable',
+  'capacitores',
+  'powerConverter', -- power_converter
+  'powerDistributor', -- power_distributor
 }
 
 Mods = {
   'component', -- GregTech
+  'info_panel', -- Nuclear Control 2
+}
+
+Blank = {
+  '___',
 }
 
