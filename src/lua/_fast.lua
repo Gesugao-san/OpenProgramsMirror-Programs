34cea9fc365b5897b8b53bf54ee47a40cb27ec84
Gesugao-san <gesugao.san@gmail.com>
2022-07-12 22:15:16 +0300

    Update _fast.lua

--- src/lua/_fast.lua
+++ src/lua/_fast.lua
@@ -1,5 +1,6 @@
 
 --lua
-local c = require('component')
-local s = require('serialization')
-print(s.serialize(c.getPrimary('type').methods))
+C = require('component')
+S = require('serialization')
+print(S.serialize(C.methods(C.getPrimary('type').address)))
+print(S.serialize(C.getPrimary('type').address))
