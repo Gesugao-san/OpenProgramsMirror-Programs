992e86d222d6ed42e7d32f9a0fcb3f985434f8ca
Gesugao-san <gesugao.san@gmail.com>
2022-07-12 23:01:18 +0300

    Add components_db.lua

---
+++ src/lua/components_db.lua
@@ -0,0 +1,21 @@
+
+-- Lua 5.2; OpenComputers 1.6.1.11;
+
+Standart = {
+  'keyboard',
+  'disk_drive',
+  'computer',
+  'filesystem',
+  'gpu',
+  'eeprom',
+  'screen',
+}
+
+Additional = {
+  'redstone',
+}
+
+Mods = {
+  'component', -- GregTech
+}
+
