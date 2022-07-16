b377173aaabb63d2cb229171a6c2a62adf7726fd
Gesugao-san <gesugao.san@gmail.com>
2022-07-17 01:59:24 +0300

    Update hologram.lua

--- src/lua/hologram.lua
+++ src/lua/hologram.lua
@@ -89,7 +89,12 @@ else
   print('setRotationSpeed - Error?')
 end
 
-local function block_make(offset_x, offset_y, offset_z, data)
+-- For Visual Studio Code, "Undefined field `sleep`."
+local function sleep(data)
+  os.sleep(data)
+end
+
+local function block_make(offset_x, offset_y, offset_z, data, sleep)
   for x = offset_x + 1, offset_x + 16, 1 do
     for y = offset_y + 9, offset_y + 24, 1 do
       for z = offset_z + 1, offset_z + 16, 1 do
@@ -97,11 +102,11 @@ local function block_make(offset_x, offset_y, offset_z, data)
         -- if (math.fmod(x, 4) == 0) then end
       end
     end
-    os.sleep(0.01)
+    sleep(sleep)
   end
 end
 
-local function block_empty(offset_x, offset_y, offset_z)
+local function block_empty(offset_x, offset_y, offset_z, sleep)
   for x = offset_x + 2, offset_x + 15, 1 do
     for y = offset_y + 10, offset_y + 23, 1 do
       for z = offset_z + 2, offset_z + 15, 1 do
@@ -109,32 +114,85 @@ local function block_empty(offset_x, offset_y, offset_z)
         -- if (math.fmod(x, 4) == 0) then end
       end
     end
-    os.sleep(0.01)
+    sleep(sleep)
   end
+end
+
+local function field_make(size_x, size_y, size_z, data, sleep)
+  for offset_x = 0, size_x, block_length do
+    for offset_y = 0, size_y, block_length do
+      for offset_z = 0, size_z, block_length do
+        block_make(offset_x, offset_y, offset_z, data, sleep)
+        block_empty(offset_x, offset_y, offset_z, sleep)
+      end
+    end
+  end
+end
+
+local function field_clear(size_x, size_y, size_z, sleep)
+  field_make(size_x, size_y, size_z, 0, sleep)
+end
+
+local function show_axis(block_length)
+  local center_xz = ((block_length * 3) / 2)
+  local center_y = (block_length / 2)
+  --local axies_x_letter_offset = center_xz + 5
+  Target1.set(center_xz, center_y, center_xz, true)
+  sleep(1)
+
+  Target1.set(center_xz - 4,     center_y + 2,     center_xz, true)
+  Target1.set(center_xz - 4 + 2, center_y + 2,     center_xz, true)
+  Target1.set(center_xz - 4 + 1, center_y + 2 + 1, center_xz, true)
+  Target1.set(center_xz - 4,     center_y + 2 + 2, center_xz, true)
+  Target1.set(center_xz - 4 + 2, center_y + 2 + 2, center_xz, true)
+  for x = center_xz, -3, -1 do
+    Target1.set(math.abs(x), center_y, center_xz, true)
+  end
+  sleep(1)
+
+  Target1.set(center_xz, center_y + 2,     center_xz - 4,     true)
+  Target1.set(center_xz, center_y + 2 + 1, center_xz - 4,     true)
+  Target1.set(center_xz, center_y + 2 + 2, center_xz - 4 + 1, true)
+  Target1.set(center_xz, center_y + 2 + 2, center_xz - 4 - 1, true)
+  for z = center_xz, -3, -1 do
+    Target1.set(center_xz, center_y, math.abs(z), true)
+  end
+  sleep(1)
+
+  Target1.set(center_xz - 4,     center_y + 6,     center_xz, true)
+  Target1.set(center_xz - 4 + 1, center_y + 6,     center_xz, true)
+  Target1.set(center_xz - 4 + 2, center_y + 6,     center_xz, true)
+  Target1.set(center_xz - 4 + 1, center_y + 6 + 1, center_xz, true)
+  Target1.set(center_xz - 4,     center_y + 6 + 2, center_xz, true)
+  Target1.set(center_xz - 4 + 1, center_y + 6 + 2, center_xz, true)
+  Target1.set(center_xz - 4 + 2, center_y + 6 + 2, center_xz, true)
+  for y = center_y, 20, 1 do
+    Target1.set(center_xz, y, center_xz, true)
+  end
+  sleep(0.1)
+
+  Target1.set(center_xz, center_y, center_xz, false)
+end
+
+
+function Main()
+  local block_count = block_length * 2
+  field_make(block_count, 0, block_count, true, 0.01) -- solid
+  sleep(1)
+  field_clear(block_count, 0, block_count, 0.01)
+  sleep(1)
+  field_make(block_count, 0, block_count, false, 0.01) -- random
+  sleep(3)
+  field_clear(block_count, 0, block_count, 0.01)
   
+  Target1.clear()
 end
 
-
-local block_count = block_length * 2
-for offset_x = 0, block_count, block_length do
-  for offset_y = 0, 0, block_length do
-    for offset_z = 0, block_count, block_length do
-      block_make(offset_x, offset_y, offset_z)
-      block_empty(offset_x, offset_y, offset_z)
-    end
-  end
-end
-
-os.sleep(3)
-
-for offset_x = 0, block_count, block_length do
-  for offset_y = 0, 0, block_length do
-    for offset_z = 0, block_count, block_length do
-      block_make(offset_x, offset_y, offset_z, 0)
-    end
-  end
-end
 
 Target1.clear()
+show_axis(block_length)
+sleep(10)
+Target1.clear()
+--Main()
 
 
