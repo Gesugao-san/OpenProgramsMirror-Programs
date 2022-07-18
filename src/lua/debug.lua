f68f3dd0b618df3f7531fdea3e980f6ba0486ff3
Gesugao-san <gesugao.san@gmail.com>
2022-07-18 11:15:29 +0300

    Add debug.lua

---
+++ src/lua/debug.lua
@@ -0,0 +1,36 @@
+
+-- Lua 5.2; OpenComputers 1.6.1.11;
+-- rm 1.lua&&edit 1.lua&&1
+
+C = require('component'); S = require('serialization')
+local debug_card = C.debug
+
+function debug(type)
+  if type == nil then print('no "' .. type .. '"'); do return end end
+  if next(C.list(type)) == nil then print('no "' .. type .. '" in list'); do return end end
+  X = C.getPrimary(type)
+  if X == nil then print('no getPrimary "' .. type .. '"'); do return end end
+  print('type: "' .. type .. '", address: ' .. X.address)
+  M = C.methods(X.address)
+  print('Methods: ' .. S.serialize(M))
+  MM = {}; N = 0
+  for k, v in pairs(M) do N = N + 1; MM[N] = k end
+  print('Methods: ' .. S.serialize(MM):gsub('{', ''):gsub('}', ''))
+  --write_to_file('/home/_fast.txt', 'Methods: ' .. S.serialize(MM))
+  for method, _ in pairs(C.methods(X.address)) do
+    print((method or 'no method') .. ': ')
+    print((C.invoke(X.address, method, 1 or '1') or 'no value'))
+    --print(C.doc(X.address, method))
+  end
+  for field, _ in pairs(C.fields(X.address)) do
+    print(field)
+  end
+  X = nil
+end
+
+print('debug_card.getPlayers(): ' .. S.serialize( debug_card.getPlayers()) )
+print('debug_card.getWorlds(): ' .. S.serialize( debug_card.getWorlds()) )
+print('debug_card.getPlayers()[1]: ' .. S.serialize( debug_card.getPlayers()[1]) )
+print('debug_card.getPlayer(): ')
+print(debug_card.getPlayer(S.serialize( debug_card.getPlayers()[1]) ))
+--debug('debug')
