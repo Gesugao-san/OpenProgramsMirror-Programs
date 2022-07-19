8037c3a748e700568c079bdf495277185dc3ed2c
Gesugao-san <gesugao.san@gmail.com>
2022-07-19 12:46:23 +0300

    Add inventory_controller.lua

---
+++ src/lua/inventory_controller.lua
@@ -0,0 +1,23 @@
+
+-- Lua 5.2; OpenComputers 1.6.1.11;
+-- rm 1.lua&&edit 1.lua&&1
+
+local component, term, s, io = require('component'), require('term'), require('serialization'), require('io')
+local inventory_controller = component.inventory_controller
+if inventory_controller == nil then do return end end
+
+
+print('inventory_controller methods: ' .. s.serialize(component.methods(inventory_controller.address)))
+
+local side = -1
+print('Searching side...')
+for i = 0, 5, 1 do
+  local InventorySize = inventory_controller.getInventorySize(i)
+  print('InventorySize(' .. i .. '): ' .. s.serialize(InventorySize))
+end
+
+if (side == -1) then
+  print('Error: Can\'t find side.')
+  return
+end
+
--- src/lua/tank_controller.lua
+++ src/lua/tank_controller.lua
@@ -2,26 +2,25 @@
 -- Lua 5.2; OpenComputers 1.6.1.11;
 -- rm 1.lua&&edit 1.lua&&1
 
-local component, term, s, io = require('component'), require('term'), require('serialization'), require('io')
+local component, sides, s, io = require('component'), require('sides'), require('serialization'), require('io')
 local tank_controller = component.tank_controller
 if tank_controller == nil then do return end end
 
 
-print('tank_controller: ' .. s.serialize(tank_controller))
-print('methods: ' .. s.serialize(component.methods(tank_controller.address)))
+print('tank_controller methods: ' .. s.serialize(component.methods(tank_controller.address)))
 
 local side = -1
 print('Searching side...')
 for i = 0, 5, 1 do
   local FluidInTank, TankCapacity, TankLevel = tonumber(tank_controller.getFluidInTank(i)['n']), tonumber(tank_controller.getTankCapacity(i)), tonumber(tank_controller.getTankLevel(i))
   if ((FluidInTank + TankCapacity + TankLevel) > 0) then
+    print('Side found! Side: ' .. sides[i] .. ' (' .. i .. ')')
     side = i
-    print('Side found! Side: ' .. i)
-    print('getFluidInTank(' .. i .. '): ' .. s.serialize(FluidInTank))
-    print('getTankCapacity(' .. i .. '): ' .. s.serialize(TankCapacity))
-    print('getTankLevel(' .. i .. '): ' .. s.serialize(TankLevel))
-    break
+    --break
   end
+  print('getFluidInTank(' .. i .. '): ' .. s.serialize(FluidInTank))
+  print('getTankCapacity(' .. i .. '): ' .. s.serialize(TankCapacity))
+  print('getTankLevel(' .. i .. '): ' .. s.serialize(TankLevel))
 end
 
 if (side == -1) then
